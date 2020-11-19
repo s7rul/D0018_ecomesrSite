c307c632c26d0468306c2b875cb5af4cd7aa4dd6
Erik Serrander <s7rul@ludd.ltu.se>
2020-11-19 15:39:48 +0100

    add to basket fix

--- purshese.py
+++ purshese.py
@@ -16,7 +16,7 @@ def createBasket(userID):
             check = cur.fetchone()
             if check != None:
                 return
-            cur.execute(" SET @id = IF(EXISTS(SELECT ID FROM Basket), ((SELECT MAX(ID) FROM Basket) + 1), 0);")
+            cur.execute("SET @id = IF(EXISTS(SELECT ID FROM Basket), ((SELECT MAX(ID) FROM Basket) + 1), 0);")
             cur.execute("INSERT INTO Basket( ID, CustomerID) VALUES (@id, %s);", (userID,))
         con.commit()
 
@@ -33,59 +33,15 @@ def addToBasket(whiskyID, count):
     if userID == None:
         return False
 
-    con = getConnection()
-    try:
+    createBasket(userID)
 
-        with con.cursor() as cur:
-
-            cur.execute("SELECT ID FROM Basket WHERE CustomerID=%s;", (userID,))
-
-            basketID = cur.fetchone()
-
-    finally:
-
-        con.close()
-
-    if basketID == None:
-        createBasket(userID)
-
-        con = getConnection()
-        try:
-
-            with con.cursor() as cur:
-
-                cur.execute("SELECT ID FROM Basket WHERE CustomerID=%s;", (userID,))
-
-                basketID = cur.fetchone()
-
-        finally:
-
-            con.close()
-
-    basketID = basketID['ID']
     con = getConnection()
     try:
         with con.cursor() as cur:
 
-            cur.execute("SELECT MAX(ID) FROM BasketProduct;")
-
-            maxID = cur.fetchone()
-            maxID = maxID['MAX(ID)']
-
-    finally:
-
-        con.close()
-
-    if maxID == None:
-        maxID = 0;
-
-    con = getConnection()
-    try:
-
-        with con.cursor() as cur:
-            maxID = maxID + 1
-
-            cur.execute("INSERT INTO BasketProduct( ID, Quantity, BasketID, ProductNumber) VALUES (%s, %s, %s, %s)", (maxID, count, basketID, whiskyID))
+            cur.execute("SET @id = IF(EXISTS(SELECT ID FROM Basket), ((SELECT MAX(ID) FROM BasketProduct) + 1), 0);")
+            cur.execute("SET @basketid = (SELECT ID from Basket WHERE CustomerID = %s);", (userID,))
+            cur.execute("INSERT INTO BasketProduct( ID, Quantity, BasketID, ProductNumber) VALUES (@id, %s, @basketid, %s)", (count, whiskyID))
         con.commit()
 
     finally:
